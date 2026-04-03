8f0e759e13dfc1af98fe7bf011af89f33101a01f
Kevin Sanchez <115368041+KevinSanchez96@users.noreply.github.com>
2026-04-03 19:08:52 -0300

    Cambio de gravedad y Suspension en el aire

--- movimiento-2d-en-godot/character_body_2d.gd
+++ movimiento-2d-en-godot/character_body_2d.gd
@@ -5,24 +5,37 @@ const JUMP_VELOCITY = -400.0
 
 
 const MAX_JUMPS = 2
+const MAX_TIEMPO_VUELO = 1.5
+
 var volando = false
-var gravedad_volando = 200
+var gravedad_volando = 30
 var jump_count = 0
+var tiempo_vuelo = 0
+var activar_vuelo = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	if Input.is_action_just_pressed("jump") and jump_count == 2 and not volando:
+		volando = true
+		velocity.y = 0
+		
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if volando and tiempo_vuelo<MAX_TIEMPO_VUELO:
+			velocity.y = 0
+			tiempo_vuelo +=delta
+		else:
+			velocity += get_gravity() * delta
+		
 	if is_on_floor():
 		jump_count = 0
 		volando = false
-	
-
+		tiempo_vuelo = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and jump_count < MAX_JUMPS:
+	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
 		jump_count += 1
 		velocity.y = JUMP_VELOCITY
 		
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
