2426db0afbe4bfa20998c8cb1a68ee0f8ed17b73
Kevin Sanchez <115368041+KevinSanchez96@users.noreply.github.com>
2026-04-03 17:46:03 -0300

    Doble salto
    Modificamos el codigo del salto , agregando un maximo de salto (2) y haciendo que al apretar Space no aumente un contador de salto cuya cantidad nunca va a superar al maximo de saltos.

--- movimiento-2d-en-godot/character_body_2d.gd
+++ movimiento-2d-en-godot/character_body_2d.gd
@@ -2,20 +2,27 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const JUMP_COUNT = 0
+
+
 const MAX_JUMPS = 2
 var volando = false
 var gravedad_volando = 200
-
+var jump_count = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if is_on_floor():
+		jump_count = 0
+		volando = false
+	
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < MAX_JUMPS:
+		jump_count += 1
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
