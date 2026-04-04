2a45a2a428acefec04e70788a061fe68cdc5580d
Kevin Sanchez <115368041+KevinSanchez96@users.noreply.github.com>
2026-04-04 18:24:03 -0300

    Modificaciones en salto y gravedad

--- movimiento-2d-en-godot/character_body_2d.gd
+++ movimiento-2d-en-godot/character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 260.0
-const JUMP_VELOCITY = -450.0
-const GRAVITY = 900.0
+const JUMP_VELOCITY = -850.0
+const GRAVITY = 2000.0
 
 const MAX_JUMPS = 2
 const MAX_TIEMPO_VUELO = 1.2
@@ -13,27 +13,31 @@ var tiempo_vuelo = 0
 
 func _physics_process(delta: float) -> void:
 	
+	#Se genera un acumulador de saltos y se limita la cantidad de saltos en el if
+	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
+		jump_count += 1
+		velocity.y = JUMP_VELOCITY
 	#Se genera el estado de "volando" y se limita la velocidad en y
 	if Input.is_action_just_pressed("jump") and jump_count == 2 and not volando:
 		volando = true
-		velocity.y = 0
 	
 	#Es lo que sucede una vez esta en el aire . Si el estado "volando" esta activo , entonces empieza a contar "tiempo_vuelo"
 	if not is_on_floor():
-		if volando and tiempo_vuelo<MAX_TIEMPO_VUELO:
+		if volando and tiempo_vuelo<MAX_TIEMPO_VUELO and Input.is_action_pressed("jump"):
 			velocity.y = 0
 			tiempo_vuelo +=delta
 		else:
-			velocity.y += GRAVITY * delta
+			volando=false
+			if velocity.y <0:
+				velocity.y += GRAVITY  * delta
+			else:
+				velocity.y += GRAVITY * 2 *delta
+			
 	#Una vez llega al piso, se resetean valores
 	if is_on_floor():
 		jump_count = 0
 		volando = false
 		tiempo_vuelo = 0
-	#Se genera un acumulador de saltos y se limita la cantidad de saltos en el if
-	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
-		jump_count += 1
-		velocity.y = JUMP_VELOCITY
 		
 	# Unicamente se cambia los nombres de las acciones
 	var direction := Input.get_axis("move_left", "move_right")
