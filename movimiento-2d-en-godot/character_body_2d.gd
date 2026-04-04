ee26b692026af92d7cfe977fe54b15d8fc855d1d
Kevin Sanchez <115368041+KevinSanchez96@users.noreply.github.com>
2026-04-03 21:12:09 -0300

    Comentarios

--- movimiento-2d-en-godot/character_body_2d.gd
+++ movimiento-2d-en-godot/character_body_2d.gd
@@ -13,30 +13,29 @@ var tiempo_vuelo = 0
 
 func _physics_process(delta: float) -> void:
 	
+	#Se genera el estado de "volando" y se limita la velocidad en y
 	if Input.is_action_just_pressed("jump") and jump_count == 2 and not volando:
 		volando = true
 		velocity.y = 0
-		
+	
+	#Es lo que sucede una vez esta en el aire . Si el estado "volando" esta activo , entonces empieza a contar "tiempo_vuelo"
 	if not is_on_floor():
 		if volando and tiempo_vuelo<MAX_TIEMPO_VUELO:
 			velocity.y = 0
 			tiempo_vuelo +=delta
 		else:
 			velocity.y += GRAVITY * delta
-		
+	#Una vez llega al piso, se resetean valores
 	if is_on_floor():
 		jump_count = 0
 		volando = false
 		tiempo_vuelo = 0
-	# Handle jump.
+	#Se genera un acumulador de saltos y se limita la cantidad de saltos en el if
 	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
 		jump_count += 1
 		velocity.y = JUMP_VELOCITY
 		
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Unicamente se cambia los nombres de las acciones
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
